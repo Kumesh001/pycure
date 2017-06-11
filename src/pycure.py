47294af0b8eb6871963723dafafc7e5dd041c299
gSilas <d.micheel@outlook.com>
2017-06-11 18:39:44 +0200

    cluster not even once

--- src/pycure.py
+++ src/pycure.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from copy import deepcopy
 from scipy.io import arff
 from os import listdir
 from os.path import isfile, join
@@ -28,15 +29,18 @@ class Cluster:
 
 
 class Cure:
-    def __init__(self, data, number_of_clusters, alpha):
+    def __init__(self, data, number_of_clusters, alpha, c):
 
         # data is a Numpy-array with rows as points and k
         # is the number of clusters
         self.data = data
         self.k = number_of_clusters
         self.alpha = alpha
+        self.c = c
         # Stores representatives for each cluster
         self.KDTree = KDTree(data)
+
+        self.shape = data.shape
 
         # Initializes each point as a Cluster object
         data_as_clusters = [Cluster(point) for point in data]
@@ -44,9 +48,10 @@ class Cure:
         # Initializes each Clusters closest Cluster and distance using the
         # KDTree
         for cluster in data_as_clusters:
-            query = KDTree.query(cluster.points[0])
-            cluster.distance_closest = query[0]
-            cluster.closest = data_as_clusters[query[1]]
+            query = self.KDTree.query(cluster.points[0], 2)
+
+            cluster.distance_closest = query[0][1]
+            cluster.closest = data_as_clusters[query[1][1]]
 
         # Stores an entry for each cluster sorted by their distances to their
         # closest cluster
@@ -60,24 +65,33 @@ class Cure:
             cluster_u = self.Heap[0]
             cluster_v = cluster_u.closest
             
-
             # remove to be merged elements from the heap and resort heap
-            remove_element(self.Heap, cluster_v)
-            remove_element(self.Heap, cluster_u)
+            self.Heap.remove(cluster_v)
+            self.Heap.remove(cluster_u)
 
             # merge the clusters to form a new cluster
-            cluster_w = merge_cluster(cluster_u, cluster_v)
+            cluster_w = self.merge_cluster(cluster_u, cluster_v)
 
             # TODO: delete rep entries from kdtree and add w representative
+            tree_data = np.empty(shape=(1, self.shape[1]))
+
+            for cluster in self.Heap:
+                for rep in cluster.rep:
+                    np.concatenate((tree_data, [rep]))
+
+            for rep in cluster_w.rep:
+                np.concatenate((tree_data, [rep]))
+
+            self.KDTree = KDTree(np.matrix(tree_data))
 
             # select arbitrary element from the heap
             cluster_w.closest = self.Heap[0]
-            cluster_w.distance_closest = distance_func(cluster_w.center,
+            cluster_w.distance_closest = self.distance_func(cluster_w.center,
                 cluster_w.closest.center)
 
             for cluster in self.Heap:
 
-                dist = distance_func(cluster_w.center, cluster.center)
+                dist = self.distance_func(cluster_w.center, cluster.center)
 
                 if (dist < cluster_w.distance_closest):
 
@@ -89,8 +103,7 @@ class Cure:
                     if(cluster.distance_closest < dist):
                         # get closest element to cluster with maximum distance
                         # dist TODO: check this
-                        (cluster.closest, cluster.distance_closest)=KDTree.query(
-                            cluster.points[0], 1, 0, 2, dist)
+                        (cluster.distance_closest, cluster.closest) = self.closest_cluster(cluster, dist)
 
                     if (cluster.closest is None):
                         cluster.closest=cluster_w
@@ -107,54 +120,63 @@ class Cure:
             self.Heap.append(cluster_w)
             self.Heap.sort(key=lambda x: x.distance_closest, reverse=False)
 
+        return 0
+
     def merge_cluster(self, cluster1, cluster2):
         # TODO: add merge function from paper
-        merged_cluster = union_func(self,cluster1,cluster2)
+        merged_cluster = self.union_func(cluster1,cluster2)
         merged_cluster.center=(len(cluster1.points)*cluster1.center+len(cluster2.points)*cluster2.center)/(len(cluster1.points)+len(cluster2.points))
-        tmpSet
-        for i in merged_cluster.rep:
+        tmpSet = []
+        merged_cluster.rep = []
+        for i in range(0, self.c):
             maxDist=0
-            for point in merged_cluster:
-               if  i == 1:
-                minDist=distance_func(point,merged_cluster.center)
-               else:     
-                for pointQ in tmpSet:
-                    tmpDist= distance_func(point,pointQ)
-                    if minDist>= tmpDist:
-                        minDist=tmpDist
+            maxPoint = []
+            for point in merged_cluster.points:
+                if  i == 0:
+                    minDist=self.distance_func(point,merged_cluster.center)
 
-                if minDist >= maxDist:
-                    maxDist = minDist
-                    maxPoint=pointQ
+                else:     
+                    tmpDist=min([self.distance_func(point,p) for p in tmpSet])
 
-            tmpSet.point[len(tmpSet.point)]=maxPoint
+                if minDist>=maxDist:
+                    maxDist=minDist
+                    maxPoint=point
+
+            # f(np.where(tmpSet==maxPoint).all(axis=1)) :
+            tmpSet.append(maxPoint)
            
-        for point in tmpSet:
+        for i in xrange(0,len(tmpSet)):
             #Smaller alpha shrinks the scattered points and favors elongated clusters
             #large alph-> scattered points get closer to mean,  cluster tend to be more compact
-            merged_cluster.rep[len(merged_cluster.rep)]=point+(merged_cluster.center-point)*self.alpha
-   
+            merged_cluster.rep.insert(i,(tmpSet[i]+(merged_cluster.center-tmpSet[i])*self.alpha))
+
         return merged_cluster
-
-
-    def remove_element(self, arr, element):
-        index=arr.index(element)
-        arr[index:index + 1]=[]
-
     
     def union_func(self,cluster1,cluster2):
-        union_cluster
-        unionpointer=len(cluster1.points)
-        for i in xrange(0,len(cluster1.points)-1):
-            union_cluster.point[i]=cluster1.point[i]
-        for i in xrange(0,len(cluster2.points)-1):
-            union_cluster.point[unionpointer+i]=cluster2.point[i]        
+        union_cluster = Cluster()
+        union_cluster.points = deepcopy(cluster1.points)
+        union_cluster.points.append(cluster2.points)
         return union_cluster
 
-              
+    def closest_cluster(self, cluster, dist):
+        
+        distance = dist
+        closest_rep = []
+
+        for representative in cluster.rep:
+            query = self.KDTree.query(representative, 2, 0, 2, dist)
+            if (query[0][1] < distance):
+                distance = query[0][1]
+                closest_rep = self.KDTree.data[query[1][1]]
+
+        for clusterz in self.Heap:
+            for point in clusterz.rep:
+                if(np.array_equal(closest_rep,point)):
+                    return (distance,clusterz)
 
     def distance_func(self, p1, p2):
         return distance.euclidean(p1, p2)
+
 
 def __load_file(path):
     """
@@ -165,9 +187,17 @@ def __load_file(path):
     """
     data, meta = arff.loadarff(open(path, 'r'))
     if data.shape == (0,):
-        return numpy.empty((0, len(meta._attributes))), 0
+        return np.empty((0, len(meta._attributes))), 0
     else:
-        return data[meta.names()[:-1]].reshape(data.shape+(-1,)), data.shape[0]
+        data_matrix = np.zeros(shape=(data.shape[0], len(data[0])))
+
+        for i in range(len(data)):
+            arff_row = data[i]
+
+            for j in range(len(arff_row) - 1):
+                data_matrix[i][j] = arff_row[j]
+
+        return data_matrix, data.shape[0]
 
 
 if __name__ == '__main__':
@@ -176,12 +206,13 @@ if __name__ == '__main__':
     file_name = str(sys.argv[1])
     number_of_clusters = int(sys.argv[2])
     alpha = float(sys.argv[3])
+    c = int(sys.argv[4])
 
     data,length = __load_file(file_name)
-    print data
-    np.matrix(data).astype(np.float)
-    print data
-    cure = Cure(data, number_of_clusters, alpha)
+
+    np.asarray(data).astype(np.float)
+
+    cure = Cure(data, number_of_clusters, alpha, c)
     list_of_labels = cure.cure_clustering()
 
     print(str(list_of_labels))
